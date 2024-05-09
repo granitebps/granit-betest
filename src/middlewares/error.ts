f3343936d1ace3134f5e624a23bad5ba5605e67a
Granite Bagas <granitebagas28@gmail.com>
2024-05-09 13:42:31 +0700

    refactor: Add validation middleware and update error middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,13 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions/httpException';
+import { ValidationException } from '../exceptions/validationException';
+import { env } from '../config/env';
 
 export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
   next(new HttpException(404, `Not Found - ${req.method} ${req.originalUrl}`));
 };
 
-export const errorHandler = (err: HttpException, req: Request, res: Response, next: NextFunction) => {
-  res.status(err.code || 500).json({
-    success: false,
-    message: err.message || 'Internal Server Error',
-  });
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof ValidationException) {
+    res.status(err.code).json({
+      success: false,
+      message: err.message,
+      fields: err.fields,
+    });
+  } else if (err instanceof HttpException) {
+    res.status(err.code).json({
+      success: false,
+      message: err.message,
+    });
+  } else {
+    console.error(err);
+    let message = err.message;
+    if (env.NODE_ENV === 'production') {
+      message = 'Internal Server Error';
+    }
+    res.status(500).json({
+      success: false,
+      message,
+    });
+  }
 };
